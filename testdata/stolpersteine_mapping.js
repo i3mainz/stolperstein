ae5e2b1514cb8901e871e9b1f8f72f319d1f67a4
situx <timo.homburg@gmx.de>
2019-05-07 21:34:55 +0200

    Added stolperstein layer

---
+++ testdata/stolpersteine_mapping.js
@@ -0,0 +1,9 @@
+var equivs={
+	"Adresse":{"osm":"memorial:addr","wikidata":"http://www.wikidata.org/prop/direct/P969"},
+	"Name":{"osm":"name","wikidata":"http://www.w3.org/2000/01/rdf-schema#label"},
+	"Inschrift":{"osm":"memorial:text","wikidata":"http://www.wikidata.org/prop/direct/P1684"},
+	"Bild":{"osm":"image","wikidata":"http://www.wikidata.org/prop/direct/P18"},
+	"Anmerkung":{"osm":"","wikidata":"http://www.w3.org/2000/01/rdf-schema#label"},
+  "Gestiftet von":{"osm":"","wikidata":"http://www.wikidata.org/prop/direct/P1028"},
+  "Verlegedatum":{"osm":"","wikidata":"http://www.wikidata.org/prop/direct/P571"}
+}
