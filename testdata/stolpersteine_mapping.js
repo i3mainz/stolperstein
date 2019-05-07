7163738972796334c0ca50028f07bf686909edfa
situx <timo.homburg@gmx.de>
2019-05-08 01:24:21 +0200

    Added Aschaffenburg

--- testdata/stolpersteine_mapping.js
+++ testdata/stolpersteine_mapping.js
@@ -1,9 +1,10 @@
 var equivs={
 	"Adresse":{"osm":"memorial:addr","wikidata":"http://www.wikidata.org/prop/direct/P969"},
-	"Name":{"osm":"name","wikidata":"http://www.w3.org/2000/01/rdf-schema#label"},
+	"Standort":{"osm":"memorial:addr","wikidata":"http://www.wikidata.org/prop/direct/P969"},
+    "Name":{"osm":"name","wikidata":"http://www.w3.org/2000/01/rdf-schema#label"},
 	"Inschrift":{"osm":"memorial:text","wikidata":"http://www.wikidata.org/prop/direct/P1684"},
-	"Bild":{"osm":"image","wikidata":"http://www.wikidata.org/prop/direct/P18"},
-	"Anmerkung":{"osm":"","wikidata":"http://www.w3.org/2000/01/rdf-schema#label"},
+	"Leben":{"osm":"","wikidata":"http://www.w3.org/2000/01/rdf-schema#comment"},
+	"Anmerkung":{"osm":"","wikidata":"http://www.w3.org/2000/01/rdf-schema#comment"},
   "Gestiftet von":{"osm":"","wikidata":"http://www.wikidata.org/prop/direct/P1028"},
   "Verlegedatum":{"osm":"","wikidata":"http://www.wikidata.org/prop/direct/P571"}
 }
