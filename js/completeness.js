0391fbcef1ad363e9cbe873e73398c5fd48c36bb
situx <timo.homburg@gmx.de>
2019-10-04 00:55:41 +0200

    Added completeness score

---
+++ js/completeness.js
@@ -0,0 +1,4 @@
+var completeness={
+    "osm":{"memorial:type":"stolperstein","memorial:text":"*","image":"_","historic":"memorial","name":"*","wikidata":"*"},
+    "wikidata":{"P31":"Q26703203","P131":"*","P18":"*","P547":"_","P625":"*","P17":"*","P6375":"*","P1684":"*","P2441":"_","P793":"Q3010369","P170":"Q195796","P186":"Q39782","P361":"Q314003","P1893":"_"}
+} 
